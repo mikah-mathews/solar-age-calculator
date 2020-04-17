6e876a8d1c062eb879dc462a74cc72a02c48993d
Mikah Mathews <mikah@outofband.org>
2020-04-17 10:31:10 -0700

    Write working test for collecting age

--- __tests__/age-planet-calculator.test.js
+++ __tests__/age-planet-calculator.test.js
@@ -5,9 +5,9 @@ describe ('Calculator', function() {
 
   test('Should return user age', () => {
     var mikahAge = new UserAge(5, 7, 2000);
-    expect(mikahAge.month).toEqual(2000);
-    expect(mikahAge.day).toEqual(5);
-    expect(mikahAge.year).toEqual(7);
+    expect(mikahAge.month).toEqual(5);
+    expect(mikahAge.day).toEqual(7);
+    expect(mikahAge.year).toEqual(2000);
     // this should be failing
   });
 
