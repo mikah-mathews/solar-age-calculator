e514000c214e97617966bd8eb9967afe0716cb11
Mikah Mathews <mikah@outofband.org>
2020-04-17 16:34:55 -0700

    Have two working tests now

--- __tests__/age-planet-calculator.test.js
+++ __tests__/age-planet-calculator.test.js
@@ -11,53 +11,55 @@ describe ('Calculator', function() {
   });
   test('Get how many Earth Days Old the User is', () => {
     let mikahDate = new UserAge(5, 7, 2000);
-    let mikahDaysOld = mikahDate.EdO();
-    expect(mikahDaysOld).toEqual(0);
-    // expect wrong answer
+    let veronicaDate = new UserAge(2, 1, 1978);
+    let mikahDaysOld = mikahDate.EdO(5, 7, 2000);
+    let veronicaDaysOld = veronicaDate.EdO(2, 1, 1978)
+    expect(mikahDaysOld).toEqual(7283);
+    expect(veronicaDaysOld).toEqual(15416);
   });
-  test('Return age in Mercury Years', () => {
-    let mikahDate = new UserAge(5, 7, 2000);
-    let mikahDaysOld = mikahDate.EdO();
-    expect()
-  });
-  test('return age in venus years', () => {
-    let mikahDate = new UserAge(5, 7, 2000);
-    let mikahDaysOld = mikahDate.EdO();
-    expect()
-  });
-  test('return age in Mars years', () => {
-    let mikahDate = new UserAge(5, 7, 2000);
-    let mikahDaysOld = mikahDate.EdO();
-    expect()
-  });
-  test('return age in Jupiter years', () => {
-    let mikahDate = new UserAge(5, 7, 2000);
-    let mikahDaysOld = mikahDate.EdO();
-    expect()
-  });
-  test('womens life expectancy returning 81', () => {
-    let mikahDate = new UserAge(5, 7, 2000);
-    let mikahDaysOld = mikahDate.EdO();
-    expect()
-  });
-  test('mens life expectancy returns 76', () => {
-    let mikahDate = new UserAge(5, 7, 2000);
-    let mikahDaysOld = mikahDate.EdO();
-    expect()
-  });
-  test('check if userAge is less than lifeExpectancy', () => {
-    let mikahDate = new UserAge(5, 7, 2000);
-    let mikahDaysOld = mikahDate.EdO();
-    expect()
-  });
-  test('if age is less than life expectancy, return remaining life', () => {
-    let mikahDate = new UserAge(5, 7, 2000);
-    let mikahDaysOld = mikahDate.EdO();
-    expect()
-  });
-  test('if age is more than life expectancy, return how much longer you have lived', () => {
-    let mikahDate = new UserAge(5, 7, 2000);
-    let mikahDaysOld = mikahDate.EdO();
-    expect()
-  });
+  // test('Return age in Mercury Years', () => {
+  //   let mikahDate = new UserAge(5, 7, 2000);
+  //   let mikahDaysOld = mikahDate.EdO();
+  //   expect()
+  // });
+  // test('return age in venus years', () => {
+  //   let mikahDate = new UserAge(5, 7, 2000);
+  //   let mikahDaysOld = mikahDate.EdO();
+  //   expect()
+  // });
+  // test('return age in Mars years', () => {
+  //   let mikahDate = new UserAge(5, 7, 2000);
+  //   let mikahDaysOld = mikahDate.EdO();
+  //   expect()
+  // });
+  // test('return age in Jupiter years', () => {
+  //   let mikahDate = new UserAge(5, 7, 2000);
+  //   let mikahDaysOld = mikahDate.EdO();
+  //   expect()
+  // });
+  // test('womens life expectancy returning 81', () => {
+  //   let mikahDate = new UserAge(5, 7, 2000);
+  //   let mikahDaysOld = mikahDate.EdO();
+  //   expect()
+  // });
+  // test('mens life expectancy returns 76', () => {
+  //   let mikahDate = new UserAge(5, 7, 2000);
+  //   let mikahDaysOld = mikahDate.EdO();
+  //   expect()
+  // });
+  // test('check if userAge is less than lifeExpectancy', () => {
+  //   let mikahDate = new UserAge(5, 7, 2000);
+  //   let mikahDaysOld = mikahDate.EdO();
+  //   expect()
+  // });
+  // test('if age is less than life expectancy, return remaining life', () => {
+  //   let mikahDate = new UserAge(5, 7, 2000);
+  //   let mikahDaysOld = mikahDate.EdO();
+  //   expect()
+  // });
+  // test('if age is more than life expectancy, return how much longer you have lived', () => {
+  //   let mikahDate = new UserAge(5, 7, 2000);
+  //   let mikahDaysOld = mikahDate.EdO();
+  //   expect()
+  // });
 });
