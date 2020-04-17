ed2604f80f419850725400618861e996a8386f8c
Mikah Mathews <mikah@outofband.org>
2020-04-17 11:35:33 -0700

    Add test for getting days

--- __tests__/age-planet-calculator.test.js
+++ __tests__/age-planet-calculator.test.js
@@ -8,8 +8,12 @@ describe ('Calculator', function() {
     expect(mikahAge.month).toEqual(5);
     expect(mikahAge.day).toEqual(7);
     expect(mikahAge.year).toEqual(2000);
-    // this should be failing
   });
-
+  test('Get how many Earth Days Old the User is', () => {
+    var mikahAge = new UserAge(5, 7, 2000);
+    var mikahDate = mikahAge.EdO();
+    var mikahDaysOld = mikahDate 
+    expect(mikahDaysOld).toEqual(7285)
+  });
 
 });
