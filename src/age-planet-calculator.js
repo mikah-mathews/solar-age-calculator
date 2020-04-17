612483cd37cabb86f38de5d5784a43022163885c
Mikah Mathews <mikah@outofband.org>
2020-04-17 16:36:35 -0700

    Have complete day counter

--- src/age-planet-calculator.js
+++ src/age-planet-calculator.js
@@ -9,17 +9,15 @@ UserAge.prototype.EdO = function(month, day, year) {
   let makeBdayDate = moment(); // sets new date
   let bYear = year // gets birth year
   let bMonth = month - 1; // gets birth month
-  let bDay = day; // gets birthday
+  let bDay = parseInt(day); // gets birthday
   makeBdayDate.year(bYear); // sets bday year
-  makeBdayDate.month(bMonth); // sets bday month
-  makeBdayDate.day(bDay); // sets bday
-  console.log(makeBdayDate.format("dddd, MMMM Do YYYY")); // console logs it all nice
+  makeBdayDate.month(bMonth); // sets bday month 
+  makeBdayDate.day(bDay - 12);// sets bday *for some reason it adds 12 so I need to subtract it??
   let makeCurrentDate = moment(); // sets a new date
   makeCurrentDate.weekday(); // sets current day
   makeCurrentDate.month(); // sets current month
   makeCurrentDate.year(); // sets current year
-  console.log(makeCurrentDate.format("dddd, MMMM Do YYYY")); // currentDate is working
-  console.log(makeCurrentDate.diff(makeBdayDate, 'days')); // counts how many days it has been since birth
+  return makeCurrentDate.diff(makeBdayDate, 'days'); // counts how many days it has been since birth
 }
 
 export function Planets(mercury, venus, mars, jupiter) {
